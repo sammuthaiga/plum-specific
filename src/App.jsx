794d28204e2fcc263520605f46ee81fdae06f59a
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-09-15 11:42:40 +0300

    feat: Initial commit
    This commit marks the initial setup of the project, including the basic folder structure, dependencies, and initial codebase. It lays the foundation for future development and enhancements

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl font-bold underline text-cyan-800">
+      Hello world!
+    </h1>
+        </div>
+    </>
+  )
+}
+
+export default App
